ba7ed17d032ad684d2005e0d0785d75fb9f05833
james lockwood <ksjjl05@gmail.com>
2019-09-19 14:34:29 -0500

    styling

--- youtube_demo/yuuvis_demo_site.py
+++ youtube_demo/yuuvis_demo_site.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def main():
     return render_template ('index.html')
 
-
-@app.route("/about")
-def about():
-    return render_template ('about.html')
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
